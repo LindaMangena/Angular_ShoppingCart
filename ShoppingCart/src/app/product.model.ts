c5da6218518d4f8dc037bb462db86ff331ecb228
Mathapelo <LindaJazz42@gmail.com>
2021-06-04 16:09:53 -0400

    Shopping cart

--- ShoppingCart/src/app/product.model.ts
+++ ShoppingCart/src/app/product.model.ts
@@ -1,4 +1,8 @@
 export class Product {
-    constructor(public id: number, public name: string, public desc: string, public price: number, public qty: number, public imgUrls: string[]
+    constructor(public id: number,
+        public name: string,
+        public desc: string, public price: number,
+        public qty: number,
+        public imgUrls: string[]
     ) { }
 }
--- ShoppingCart/src/app/product.service.ts
+++ ShoppingCart/src/app/product.service.ts
@@ -5,6 +5,40 @@ import { Product } from './product.model';
   providedIn: 'root'
 })
 export class ProductService {
-  products : Product[] =[]
-  constructor() { }
+  products : Product[] =[
+    new Product(
+      1,
+      'Electric Kettle',
+      'The one touch button design allows you to easily and safely open the lid with one hand.',
+      25,
+      1,
+      ['','','']
+    ),
+    new Product(
+      2,
+      'Wireless Mouse',
+      'The one touch button design allows you to easily and safely open the lid with one hand.',
+      25,
+      1,
+      ['','','']
+    ),
+    new Product(
+      2,
+      'Iron Stream ',
+      'Used vertically to steam cloths on hanger or to take out wrinkles from curtains and drapes',
+      15
+      ,
+      1,
+      ['','','']
+    )
+  ]
+
+  constructor(){}
+
+  getProducts(){
+    return this.products;
+
+  }
+  
 }
+
